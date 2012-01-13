e7ffa19fc973d367aeee3e6b9ea004168214158a
Stephen Lewis <stephen@experienceinternet.co.uk>
2012-01-13 01:51:08 +0000

    Tidy up Module Updater file.

--- ee2/third_party/testee/upd.testee.php
+++ ee2/third_party/testee/upd.testee.php
@@ -1,101 +1,80 @@
-<?php if ( ! defined('EXT')) exit('Invalid file request');
+<?php if ( ! defined('BASEPATH')) exit('Invalid file request');
 
 /**
- * Test-driven add-on development module.
+ * Testee module installation and update methods.
  *
- * @package		Testee
- * @author		Stephen Lewis <stephen@experienceinternet.co.uk>
- * @copyright	Experience Internet
+ * @author      Stephen Lewis (http://github.com/experience/)
+ * @copyright   Experience Internet
+ * @package     Testee
  */
 
 class Testee_upd {
-	
-	/* --------------------------------------------------------------
-	 * PUBLIC PROPERTIES
-	 * ------------------------------------------------------------ */
-	
-	/**
-	 * Module version.
-	 *
-	 * @access	public
-	 * @var 	string
-	 */
-	public $version = '';
-	
-	
-	/* --------------------------------------------------------------
-	 * PRIVATE PROPERTIES
-	 * ------------------------------------------------------------ */
-	
-	/**
-	 * ExpressionEngine object reference.
-	 *
-	 * @access	private
-	 * @var 	object
-	 */
-	private $_ee;
-	
-	
-	
-	/* --------------------------------------------------------------
-	 * PUBLIC METHODS
-	 * ------------------------------------------------------------ */
-	
-	/**
-	 * Constructor.
-	 *
-	 * @access	public
-	 * @return	void
-	 */
-	public function __construct()
-	{
-		$this->_ee =& get_instance();
-		
-		$this->_ee->load->add_package_path(PATH_THIRD .'testee/');
-		$this->_ee->load->model('testee_model');
-		
-		$this->version = $this->_ee->testee_model->get_package_version();
-	}
-	
-	
-	/**
-	 * Module installer.
-	 *
-	 * @access	public
-	 * @return	bool
-	 */
-	public function install()
-	{
-		return $this->_ee->testee_model->install_module();
-	}
-	
-	
-	/**
-	 * Module uninstaller.
-	 *
-	 * @access	public
-	 * @return	bool
-	 */
-	public function uninstall()
-	{
-		return $this->_ee->testee_model->uninstall_module();
-	}
-	
-	
-	/**
-	 * Module updater.
-	 *
-	 * @access	public
-	 * @param	string		$installed_version		The installed version.
-	 * @return	bool
-	 */
-	public function update($installed_version = '')
-	{
-		return $this->_ee->testee_model->update_module($installed_version, $this->_ee->testee_model->get_package_version());
-	}
-	
+  
+  private $EE;
+  public $version = '';
+  
+  
+  /* --------------------------------------------------------------
+   * PUBLIC METHODS
+   * ------------------------------------------------------------ */
+  
+  /**
+   * Constructor.
+   *
+   * @access  public
+   * @return  void
+   */
+  public function __construct()
+  {
+    $this->EE =& get_instance();
+    
+    $this->EE->load->add_package_path(PATH_THIRD .'testee/');
+    $this->EE->load->model('testee_model');
+    
+    $this->version = $this->EE->testee_model->get_package_version();
+  }
+  
+  
+  /**
+   * Installs the module.
+   *
+   * @access  public
+   * @return  bool
+   */
+  public function install()
+  {
+    return $this->EE->testee_model->install_module();
+  }
+  
+  
+  /**
+   * Uninstalls the module.
+   *
+   * @access  public
+   * @return  bool
+   */
+  public function uninstall()
+  {
+    return $this->EE->testee_model->uninstall_module();
+  }
+  
+  
+  /**
+   * Updates the module.
+   *
+   * @access  public
+   * @param   string    $installed_version    The installed version.
+   * @return  bool
+   */
+  public function update($installed_version = '')
+  {
+    return $this->EE->testee_model->update_module($installed_version,
+      $this->EE->testee_model->get_package_version());
+  }
+
+
 }
 
 
-/* End of file		: upd.testee.php */
-/* File location	: /system/expressionengine/third_party/testee/upd.testee.php */
+/* End of file    : upd.testee.php */
+/* File location  : third_party/testee/upd.testee.php */
