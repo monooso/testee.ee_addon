2c716470ee680c5ddbc3e49b3b6b427c3fd2e5a3
Stephen Lewis <stephen@experienceinternet.co.uk>
2012-01-13 01:46:33 +0000

    Tidy-up Module CP file.

--- ee2/third_party/testee/mcp.testee.php
+++ ee2/third_party/testee/mcp.testee.php
@@ -3,146 +3,111 @@
 /**
  * Test-driven add-on development module.
  *
- * @package		Testee
- * @author		Stephen Lewis <stephen@experienceinternet.co.uk>
- * @copyright	Experience Internet
+ * @author      Stephen Lewis (http://github.com/experience/)
+ * @copyright   Experience Internet
+ * @package     Testee
  */
 
 class Testee_mcp {
-	
-	/* --------------------------------------------------------------
-	 * PRIVATE PROPERTIES
-	 * ------------------------------------------------------------ */
-	
-	/**
-	 * Base module query string.
-	 *
-	 * @access	private
-	 * @var		string
-	 */
-	private $_base_qs = '';
-	
-	/**
-	 * Base module URL.
-	 *
-	 * @access	private
-	 * @var 	string
-	 */
-	private $_base_url = '';
-	
-	/**
-	 * ExpressionEngine object reference.
-	 *
-	 * @access	private
-	 * @var 	object
-	 */
-	private $_ee;
-	
-	
-	
-	/* --------------------------------------------------------------
-	 * PUBLIC METHODS
-	 * ------------------------------------------------------------ */
-	
-	/**
-	 * Constructor.
-	 *
-	 * @access	public
-	 * @param 	bool		$switch 	My whole life I don't know what this song means.
-	 * @return	void
-	 */
-	public function __construct($switch = TRUE)
-	{
-		$this->_ee 			=& get_instance();
-		$this->_base_qs 	= 'C=addons_modules' .AMP .'M=show_module_cp' .AMP .'module=testee';
-		$this->_base_url	= BASE .AMP .$this->_base_qs;
-		
-		// Load the model.
-		$this->_ee->load->model('testee_model');
-		
-		// Add a base breadcrumb.
-		$this->_ee->cp->set_breadcrumb($this->_base_url, $this->_ee->lang->line('testee_module_name'));
-	}
-	
-	
-	/**
-	 * Displays the default module control panel page.
-	 *
-	 * @access	public
-	 * @return	string
-	 */
-	public function index()
-	{
-		// Load our glamorous assistants.
-		$this->_ee->load->helper('form');
-		$this->_ee->load->library('table');
-		
-		// Retrieve the theme folder URL.
-		$theme_url = $this->_ee->testee_model->get_theme_url();
-		
-		// Include the main JS file.
-		$this->_ee->cp->add_to_foot('<script type="text/javascript" src="' .$theme_url .'js/cp.js"></script>');
-		$this->_ee->javascript->compile();
+  
+  private $_base_qs = '';
+  private $_base_url = '';
+  private $EE;
+  
+  
+  
+  /* --------------------------------------------------------------
+   * PUBLIC METHODS
+   * ------------------------------------------------------------ */
+  
+  /**
+   * Constructor.
+   *
+   * @access  public
+   * @param   bool    $switch   My whole life I don't know what this song means.
+   * @return  void
+   */
+  public function __construct($switch = TRUE)
+  {
+    $this->EE =& get_instance();
+    $this->EE->load->model('testee_model');
 
-		// Include the CSS.
-		$this->_ee->cp->add_to_head('<link media="screen, projection" rel="stylesheet" type="text/css" href="' .$theme_url .'css/cp.css" />');
-		
-		$vars = array(
-			'form_action'		=> $this->_base_qs .AMP .'method=run_test',
-			'cp_page_title'		=> $this->_ee->lang->line('testee_module_name'),
-			'tests'				=> $this->_ee->testee_model->get_tests()
-		);
-		
-		return $this->_ee->load->view('tests_index', $vars, TRUE);
-	}
-	
-	
-	/**
-	 * Handles a 'run_test' request.
-	 *
-	 * @access	public
-	 * @return	void
-	 */
-	public function run_test()
-	{
-		$test_path = $this->_ee->input->post('tests') OR ! is_array($test_path);
-		
-		/* @todo : create custom Testee Exception class(?) */
-		
-		// Run the tests.
-		try
-		{
-			$test_results = $this->_ee->testee_model->run_tests($test_path);
-		}
-		catch (Exception $e)
-		{
-			$this->_ee->functions->redirect($this->_base_url);
-			return;
-		}
-		
-		// Retrieve the theme folder URL.
-		$theme_url = $this->_ee->testee_model->get_theme_url();
-		
-		// Include the CSS.
-		$this->_ee->cp->add_to_head('<link media="screen, projection" rel="stylesheet" type="text/css" href="' .$theme_url .'css/cp.css" />');
-		
-		// Include the JavaScript.
-		$this->_ee->cp->add_to_foot('<script type="text/javascript" src="' .$theme_url .'js/cp.js"></script>');
-		$this->_ee->javascript->compile();
-		
-		$vars = array(
-			'form_action'		=> $this->_base_qs .AMP .'method=run_test',
-			'tests_index_url'	=> $this->_base_url,
-			'cp_page_title'		=> 'Testee Test Results',
-			'test_results'		=> $test_results,
-			'tests'				=> $test_path
-		);
-		
-		return $this->_ee->load->view('test_results', $vars, TRUE);
-	}
-	
+    $this->_base_qs = 'C=addons_modules' .AMP .'M=show_module_cp'
+      .AMP .'module=testee';
+
+    $this->_base_url = BASE .AMP .$this->_base_qs;
+    
+    $this->EE->cp->set_breadcrumb($this->_base_url,
+      $this->EE->lang->line('testee_module_name'));
+
+    // Retrieve the theme folder URL.
+    $theme_url = $this->EE->testee_model->get_theme_url();
+    
+    // Include the custom CSS and JS on all pages.
+    $this->EE->cp->add_to_head('<link rel="stylesheet" href="'
+      .$theme_url .'css/cp.css" />');
+    
+    $this->EE->cp->add_to_foot('<script src="'
+      .$theme_url .'js/cp.js"></script>');
+
+    $this->EE->javascript->compile();
+  }
+  
+  
+  /**
+   * Displays the default module control panel page.
+   *
+   * @access  public
+   * @return  string
+   */
+  public function index()
+  {
+    $this->EE->load->helper('form');
+    $this->EE->load->library('table');
+    
+    $vars = array(
+      'form_action'   => $this->_base_qs .AMP .'method=run_test',
+      'cp_page_title' => $this->EE->lang->line('testee_module_name'),
+      'tests'         => $this->EE->testee_model->get_tests()
+    );
+    
+    return $this->EE->load->view('tests_index', $vars, TRUE);
+  }
+  
+  
+  /**
+   * Handles a 'run_test' request.
+   *
+   * @access  public
+   * @return  void
+   */
+  public function run_test()
+  {
+    $test_path = $this->EE->input->post('tests') OR ! is_array($test_path);
+    
+    try
+    {
+      $test_results = $this->EE->testee_model->run_tests($test_path);
+    }
+    catch (Exception $e)
+    {
+      $this->EE->functions->redirect($this->_base_url);
+      return;
+    }
+    
+    $vars = array(
+      'form_action'     => $this->_base_qs .AMP .'method=run_test',
+      'tests_index_url' => $this->_base_url,
+      'cp_page_title'   => 'Testee Test Results',
+      'test_results'    => $test_results,
+      'tests'           => $test_path
+    );
+    
+    return $this->EE->load->view('test_results', $vars, TRUE);
+  }
+  
 }
 
 
-/* End of file		: mcp.testee.php */
-/* File location	: /system/expressionengine/third_party/testee/mcp.testee.php */
+/* End of file    : mcp.testee.php */
+/* File location  : third_party/testee/mcp.testee.php */
